624d65ddec302cb60e634b7a85cba7f4d04eb058
Randy Yee <PCX5@cdc.gov>
2020-09-28 17:41:41 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,262 @@
+library(shiny)
+library(shinythemes)
+library(shiny)
+library(readxl)
+library(ICPIHelpers)
+library(tidyverse)
+library(rpivotTable)
+library(tidytext)
+library(DT)
+library(reshape2)
+
+options(shiny.maxRequestSize=4000*1024^2)
+
+shinyServer(function(input, output) {
+  
+  #### PARAMETERS ####
+  
+  row_count <- reactive({input$narrativesdt_rows_selected})
+  operatingunit_name <-reactive({narratives()[[row_count(), 1]]}) 
+  indicator_name <- reactive({narratives()[[row_count(), 5]]})
+  im_name <- reactive({narratives()[[row_count(), 8]]})
+  support_name <- reactive({narratives()[[row_count(), 6]]})
+  
+  narratives_content <- reactive({
+          narratives()[[row_count(), 12]]
+  })
+  
+  bing <- get_sentiments("bing")%>%
+    filter(word != "positive") %>%
+    filter(word != "positives") %>%
+    filter(word != "negative") %>%
+    filter(word != "negatives") %>%
+    filter(word != "patient") %>%
+    filter(word != "achievement") %>%
+    filter(word != "emergency ward") %>%
+    mutate(sentiment = case_when(
+      word == "suppression" ~ "positive",
+      TRUE ~ sentiment
+    ))
+  
+  #### NARRATIVES IMPORT ####
+  
+  narratives <- reactive({
+    if(is.null(input$import))
+    {
+      return()
+    }
+    isolate({
+      narrative_path <- input$import
+      data <- read_excel(narrative_path$datapath,
+                         col_types = "text",
+                         skip = 7)
+    })
+  })
+  
+  
+  output$narrativesdt <- DT::renderDataTable({
+    
+    DT::datatable(narratives()[,c(1,3,5,6,7,12)], 
+                  selection = "single",
+                  rownames=FALSE,
+                  filter="top",
+                  options = list(
+                    searchHighlight = TRUE,
+                    scroller = TRUE,
+                    scrollX = TRUE,
+                    scrollY = 700
+                  
+                  )
+    )
+  })
+  
+  #### MSD ####
+  
+  data <- reactive({
+    inFile <- input$import1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    new_msd <- read_new_msd(inFile$datapath,
+                            save_rds = FALSE,
+                            remove_txt = FALSE)
+    
+    new_msd <- pivot_longer(new_msd,
+                            targets:cumulative,
+                            names_to = "period",
+                            values_to = "value")
+    
+    new_msd <- unite(new_msd, 
+                     "period", 
+                     c("fiscal_year", "period"),
+                     sep = "_", 
+                     remove = T)
+    
+  })
+  
+  #### PIVOT TABLE & VISUALS ####
+  
+  output$op <- renderText({
+    operatingunit_name()
+  })
+  
+  output$ind <- renderText({
+    indicator_name()
+  })
+  
+  output$sup <- renderText({
+    support_name()
+  })
+  
+  output$im <- renderText({
+    im_name()
+  })
+  
+  output$title <- renderText({
+    paste(operatingunit_name(),indicator_name(),support_name(),im_name(), sep = "; ")
+  })
+  
+  output$content <- renderText({
+    narratives_content()
+  })
+  
+  observeEvent(input$display,{
+    
+    
+    output$msd_df <- renderRpivotTable({
+      
+      rpivotTable(data()%>%
+                    filter(operatingunit == operatingunit_name())%>%
+                    filter(indicator == indicator_name()) %>%
+                    filter(mech_name == im_name()) %>%
+                    filter(indicatortype == support_name()) %>% 
+                    select(-contains("uid")),
+                  rows="period", cols=c("operatingunit","psnu","standardizeddisaggregate"),
+                  vals = "value", aggregatorName = "Integer Sum"
+      )
+      
+    })
+  })
+  
+  
+  ### TEXT ANALYSIS ###
+  
+  text_prepare <- reactive({
+    
+    clean_textdf <- narratives() %>%
+      unnest_tokens(word, Narrative)%>%
+      anti_join(stop_words)
+    
+  })
+  
+  sentiment_df <- reactive({
+    
+    mersentiment <- text_prepare() %>%
+      inner_join(bing) %>%
+      count(`Operating Unit`, `Indicator Bundle`, sentiment) %>%
+      spread(sentiment, n, fill = 0) %>%
+      mutate(sentiment = positive - negative)
+  })
+  
+  
+  observeEvent(input$display,{
+    
+    output$sentiment_ous <- renderPlot(
+      
+      ggplot(sentiment_df(), aes(`Indicator Bundle`, sentiment, fill = `Operating Unit`)) +
+        geom_bar(stat = "identity", show.legend = FALSE) +
+        theme(axis.text.x = element_text(size = rel(0.5))) +
+        facet_wrap(~`Operating Unit`, scales = "free_x")+
+        theme_linedraw() +
+        theme(axis.title.x = element_blank())
+      
+    )
+  })
+  
+  observeEvent(input$display,{
+    
+    output$sentiment_ou <- renderPlot(
+      
+      ggplot(sentiment_df()%>% filter(`Operating Unit`==operatingunit_name()), aes(`Indicator Bundle`, sentiment, fill = `Operating Unit`)) +
+        geom_bar(stat = "identity", show.legend = FALSE) +
+        facet_wrap(~`Operating Unit`, scales = "free_x") +
+        theme_linedraw()+
+        theme(axis.title.x = element_blank())
+      
+    )
+  })
+  
+  
+  observeEvent(input$display,{
+    
+  output$sentiment_ou_contribution <- renderPlot({
+    
+      text_prepare() %>% 
+        filter(`Operating Unit`==operatingunit_name()) %>%
+        inner_join(bing) %>%
+        count(`Indicator Bundle`, word, sentiment, sort = TRUE) %>%
+        mutate(n = ifelse(sentiment == "negative", -n, n)) %>%
+        mutate(word = reorder(word, n)) %>%
+        ggplot(aes(word, n, fill = sentiment)) +
+        geom_col() +
+        coord_flip() +
+        labs(y = "Contribution to sentiment")+
+        facet_wrap(~`Indicator Bundle`, scales = "free_y") +
+      scale_x_discrete(guide=guide_axis(n.dodge=2)) + 
+      theme_linedraw() +
+      theme(legend.position = "top", axis.title.x = element_blank())
+    })
+  })
+  
+  
+  observeEvent(input$display,{
+    
+    output$sentiment_ou_contribution_ind <- renderPlot({
+
+      text_prepare() %>% 
+        filter(`Operating Unit`==operatingunit_name()) %>%
+        filter(Indicator==indicator_name()) %>%
+        inner_join(bing) %>%
+        count(`Indicator Bundle`, Indicator, word, sentiment, sort = TRUE) %>%
+        mutate(n = ifelse(sentiment == "negative", -n, n)) %>%
+        mutate(word = reorder(word, n)) %>%
+        ggplot(aes(word, n, fill = sentiment)) +
+        geom_col() +
+        coord_flip() +
+        labs(y = "Contribution to sentiment")+
+        facet_wrap(~Indicator, scales = "free_y") +
+        scale_x_discrete(guide=guide_axis(n.dodge=2)) + 
+        theme_linedraw() +
+        theme(legend.position = "none", axis.title.x = element_blank())
+    })
+  })
+  
+  
+  ### WORD CLOUD ###
+  observeEvent(input$display,{
+    output$compare_cloud_ou <- renderPlot({
+      text_prepare() %>% 
+        filter(`Operating Unit`==operatingunit_name()) %>%
+        inner_join(bing) %>%
+        count(word, sentiment, sort = TRUE) %>%
+        acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+        comparison.cloud(colors = c("#F8766D", "#00BFC4"),
+                         max.words = 100)
+    })
+  })
+  
+  observeEvent(input$display,{
+    output$compare_cloud_ouind <- renderPlot({
+      text_prepare() %>% 
+        filter(`Operating Unit`==operatingunit_name()) %>%
+        filter(Indicator==indicator_name()) %>%
+        inner_join(bing) %>%
+        count(word, sentiment, sort = TRUE) %>%
+        acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+        comparison.cloud(colors = c("#F8766D", "#00BFC4"),
+                         max.words = 100)
+    })
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,96 @@
+library(shiny)
+library(shinythemes)
+library(shiny)
+library(readxl)
+library(ICPIHelpers)
+library(tidyverse)
+library(rpivotTable)
+library(tidytext)
+library(DT)
+library(reshape2)
+
+shinyUI(navbarPage(theme = shinytheme("simplex"),
+                   
+                   "Narratives & Visualizations",
+                   
+                   tabPanel(
+                     "Home",
+                     sidebarLayout(
+                       sidebarPanel(fileInput("import", "Choose Narratives File (xlsx)",
+                                              multiple = FALSE),
+                                    fileInput("import1", "Choose MSD File (txt)",
+                                              multiple = FALSE),
+                                    actionButton("display","Analyze!")),
+                       mainPanel(
+                         tags$h1("Narratives Triangulation Tool"),
+                         tags$h3("Introduction"),
+                         p("This tool triangulates Monitoring, Evaluation, and Reporting (MER) narratives with indicator results.
+                                  The Narratives Traingulation Tool takes in MER Narratives and MER Structured Datasets (MSD) providing cross-filtering between the two data sources.
+                                  Once the required data sources have been imported, the pivot table visualizer can be used to investigate the content of the selected narrative using the MER indicators.
+                                  This tool also has advanced features for textual analysis of the narratives including sentiment analysis, tf-idf, n-grams and correlations."),
+                         hr(),
+                         tags$h3("Instructions"),
+                         p("1. Upload your narratives file (.xlsx)"),
+                         p("2. Upload your MSD file (.txt)"),
+                         p(tags$i("Note: Any type of MSD can be used, but results will be limited to the level of the MSD")),
+                         p("3. Click Analyze!"),
+                         p("4. Choose a narrative in Narratives Explorer"),
+                         p("5. Use Narratives Trends to see any reported narratives from previous quarters"),
+                         p("6. Explore different text mining analyses under Narrative Analysis"),
+                         p("6. Visualize the narrative with MER indicators using under Result"),
+                         hr(),
+                         tags$h3("Questions?"),
+                         p("Randy Yee (pcx5@cdc.gov)"),
+                         p("CDC/GDIT | CGH | DGHT | HIDMSB | DUAT-ICPI")
+                       )
+                     )
+                     
+                   ),
+                   navbarMenu("Narratives",
+                              tabPanel("Narratives Explorer",
+                                       DT::dataTableOutput('narrativesdt')),
+                              tabPanel("Narrative Trends")
+                   ),
+                   navbarMenu("Narrative Analysis",
+                              tabPanel("Sentiment Analysis",
+                                       tags$h2("Introduction"),
+                                       p("Sentiment analysis is performed using a modified lexicon of Bing Liu and collaborators. 
+                                         Unigrams with sentiment designations are joined to the narrative datasets and scored according to pre-defined sentiments of positive or negative.
+                                         Modification have been made to the Bing lexicon so that the sentiments are in agreement with the language of PEPFAR (i.e. suppression which is commonly understood to be positive, as in viral suppression)."),
+                                       tags$h2("Global Sentiments"),
+                                       p("This section shows the general sentiment of the PEPFAR program across all operating units by indicator bundles."),
+                                       plotOutput("sentiment_ous", height = "800px"),
+                                       tags$h2("Sentiments of the Operating Unit"),
+                                       p("If the following sections are empty, please go to the Narratives page and select a narrative in the table."),
+                                       plotOutput("sentiment_ou", height = "400px"),
+                                       tags$h2("Contribution of Each Word to Sentiments"),
+                                       p("The following plots show the relative contribution of each word to the overall indicator bundle."),
+                                       plotOutput("sentiment_ou_contribution", height = "800px"),
+                                       plotOutput("sentiment_ou_contribution_ind")
+                              ),
+                              tabPanel("Wordclouds",
+                                       tags$h2("Introduction"),
+                                       p("Word clouds showing top positive and negative words. 
+                                         Size of word indicates frequency.
+                                         If the following sections are empty, please go to the Narratives page and select a narrative in the table."),
+                                       tags$h2("Operating Unit Comparison Cloud: Positive vs. Negative Sentiment"),
+                                       p("Comparison word cloud showing the top positive and negative sentiments of the selected operating unit."),
+                                       plotOutput("compare_cloud_ou"),
+                                       tags$h2("Indicator Comparison Cloud: Positive vs. Negative Sentiment"),
+                                       p("Comparison word cloud showing the top positive and negative sentiments of the selected operating unit and indicator."),
+                                       plotOutput("compare_cloud_ouind")
+                                       ),
+                              tabPanel("tf-idf"),
+                              tabPanel("N-grams and Correlations"),
+                              tabPanel("Latent Dirichlet Allocation")
+                   ),
+                   tabPanel("Results",
+                            tags$h2("Summary"),
+                            textOutput("title"),
+                            p(),
+                            textOutput("content"),
+                            hr(),
+                            tags$h2("Pivot Table"),
+                            rpivotTableOutput("msd_df"))
+)
+)
