d7ff00abcb5561b2a316fb915c668080d4f164d1
Randy Yee <PCX5@cdc.gov>
2021-02-18 15:24:28 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -8,8 +8,12 @@ library(rpivotTable)
 library(tidytext)
 library(DT)
 library(reshape2)
-library(wordcloud)
+library(wordcloud2)
 library(ggplot2)
+library(igraph)
+library(ggraph)
+library(leaflet)
+library(crayon)
 #library(reactlog)
 
 options(shiny.maxRequestSize=4000*1024^2)
@@ -17,37 +21,42 @@ options(shiny.maxRequestSize=4000*1024^2)
 
 ##### Functions ####
 source("./functions/data_management.R")
+source("./functions/bigrams_management.R")
 
 ##### Modules ####
 source("./modules/mod_home.R")
 source("./modules/mod_dashboard.R")
 source("./modules/submod_ui_updater.R")
+source("./modules/mod_mapboard.R")
 source("./modules/mod_impacttable.R")
 source("./modules/mod_resources.R")
 source("./modules/mod_triangulation.R")
 
 #### Text Resources ####
-bing <- read_sheet("https://docs.google.com/spreadsheets/d/1QAhceme-y-dJrsV4WrCk5lqzFPhLfnNfjcn9xuTHiGQ/edit?usp=sharing",
-                   sheet = "HIV Sentiments (based on Bing et al.)")
+bing <-       read_sheet("https://docs.google.com/spreadsheets/d/1QAhceme-y-dJrsV4WrCk5lqzFPhLfnNfjcn9xuTHiGQ/edit?usp=sharing",
+                         sheet = "HIV Sentiments (based on Bing et al.)")
 
-stopwords <- read_sheet("https://docs.google.com/spreadsheets/d/1QAhceme-y-dJrsV4WrCk5lqzFPhLfnNfjcn9xuTHiGQ/edit?usp=sharing",
-                        sheet = "Stopwords")
+stopwords <-  read_sheet("https://docs.google.com/spreadsheets/d/1QAhceme-y-dJrsV4WrCk5lqzFPhLfnNfjcn9xuTHiGQ/edit?usp=sharing",
+                         sheet = "Stopwords")
 
 covidwords <- read_sheet("https://docs.google.com/spreadsheets/d/1QAhceme-y-dJrsV4WrCk5lqzFPhLfnNfjcn9xuTHiGQ/edit?usp=sharing",
                          sheet = "Covidwords")
 #--------------------------------------------------------------------------------------------#
-
 #### UI: Sidebar ####
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Home", tabName = "home", icon = icon("home")),
-    menuItem("Dashboard", tabName = "analyses", icon = icon("line-chart")),
-    menuItem("Impact Table", tabName = "impacttable", icon = icon("table")),
-    menuItem("Topic Modeling", tabName = "impacttable", icon = icon("newspaper-o")),
-    menuItem("Resources", tabName = "resources", icon = icon("book")),
-    menuItem("Triangulation", tabName = "triangulate", icon = icon("puzzle-piece")),
-    fileInput("n_path", "Choose Narratives File (xlsx)", multiple = FALSE),
-    fileInput("m_path", "Choose MSD File (txt)", multiple = FALSE)
+    menuItem("Home",           tabName = "home",        icon = icon("home")),
+    menuItem("Dashboard",      tabName = "analyses",    icon = icon("line-chart")),
+    menuItem("Impact Table",   tabName = "impacttable", icon = icon("table")),
+    #menuItem("Topic Modeling", tabName = "impacttable", icon = icon("newspaper-o")),
+    menuItem("Resources",      tabName = "resources",   icon = icon("book")),
+    menuItem("Triangulation",  tabName = "triangulate", icon = icon("puzzle-piece")),
+    fileInput("n_path",
+              shiny::HTML("<span style='color: white'>Choose Narratives File (xlsx)</span>"),
+              multiple = FALSE),
+    fileInput("m_path",
+              shiny::HTML("<span style='color: white'>Choose MSD File (txt)</span>"),
+              multiple = FALSE)
   )
 )
 
@@ -56,9 +65,10 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   shinyDashboardThemes(theme = "poor_mans_flatly"),
   tabItems(
-    tabItem(tabName = "home", home_ui()),
-    tabItem(tabName = "analyses", dashboard_ui("d")),
-    tabItem(tabName = "resources", resources_ui("r")),
+    tabItem(tabName = "home",        home_ui()),
+    tabItem(tabName = "analyses",    dashboard_ui("d")),
+    tabItem(tabName = "impacttable", impact_ui("i")),
+    tabItem(tabName = "resources",   resources_ui("r")),
     tabItem(tabName = "triangulate", triangulation_ui("t"))
   )
 )
@@ -89,9 +99,10 @@ server <- function(input, output, session){
   })
   
   
-  
   # Server Modules
   dashboard_server("d", narratives_df(), bing, stopwords)
+  
+  impact_server("i", narratives_df())
   
   resources_server("r", bing, stopwords, covidwords)
   
